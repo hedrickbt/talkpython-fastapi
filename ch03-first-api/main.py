685cd2a286538b95ed8ecc40e20cc3939c82a638
bhedrick <brooke.t.hedrick@gmail.com>
2021-12-23 10:33:49 -0600

    - added index page - added arguments to the calculate api - added error handling support to the calculate api

--- ch03-first-api/main.py
+++ ch03-first-api/main.py
@@ -1,13 +1,43 @@
+import json
+from typing import Optional
+
 import fastapi
 import uvicorn
 
 api = fastapi.FastAPI()
 
 
+@api.get('/')
+def index():
+    body = """
+    <html>
+    <body style='padding: 10px;'>
+    <h1>Welcome to the API</h1>
+    <div>
+    Try it: <a href='/api/calculate/?x=7&y=11'>/api/calculate/?x=7&y=11</a>
+    </div>
+    </body>
+    </html>
+"""
+    return fastapi.responses.HTMLResponse(content=body)
+
+
 @api.get('/api/calculate')
-def calculate():
-    value = 2 + 2
+def calculate(x: int, y: int, z: Optional[int] = None):
+    if z == 0:
+        error = {
+            'error': 'ERROR: z cannot be 0.'
+        }
+        # return fastapi.Response(content=json.dumps(error), media_type='application/json', status_code=400)
+        return fastapi.responses.JSONResponse(content=error, status_code=400)
+
+    value = (x + y)
+    if z is not None:
+        value /= z
     return {
+        'x': x,
+        'y': y,
+        'z': z,
         'value': value,
     }
 
