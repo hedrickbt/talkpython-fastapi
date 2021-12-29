d8a0036f393840c60c0121edf42b140e01ee33a4
bhedrick <brooke.t.hedrick@gmail.com>
2021-12-29 09:29:26 -0600

    - calling openweather api

--- ch05-a-realistic-api/main.py
+++ ch05-a-realistic-api/main.py
@@ -1,8 +1,12 @@
+import json
+from pathlib import Path
+
 import fastapi
 import uvicorn
 from starlette.staticfiles import StaticFiles
 
 from api import weather_api
+from services import openweather_service
 from views import home
 
 api = fastapi.FastAPI()
@@ -14,8 +18,19 @@ def configure_routing():
     api.include_router(weather_api.router)
 
 
+def configure_api_keys():
+    file = Path('settings.json').absolute()
+    if not file.exists():
+        print(f'WARNING: {file} file not found, you cannot continue, please see settings_template.json')
+        raise Exception('settings.json file not found, you cannot continue, please see settings_template.json')
+    with open('settings.json') as fin:
+        settings = json.load(fin)
+        openweather_service.api_key = settings.get('api_key')
+
+
 def configure():
     configure_routing()
+    configure_api_keys()
 
 
 if __name__ == '__main__':
--- ch05-a-realistic-api/services/openweather_service.py
+++ ch05-a-realistic-api/services/openweather_service.py
@@ -1,9 +1,19 @@
 from typing import Optional
 
+import requests
+
+api_key: Optional[str] = None
+
 
 def get_report(city: str, state: Optional[str], country: str, units: str) -> dict:
-    key = 'nope'
-    q = f'{city},{country}'
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={q}&appid={key}&units={units}'
-    print(url)
+    if state:
+        q = f'{city},{state},{country}'
+    else:
+        q = f'{city},{country}'
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={q}&appid={api_key}&units={units}'
+    response = requests.get(url)
+    response.raise_for_status()
+    data = response.json()
+    forecast = data['main']
+    return forecast
 
