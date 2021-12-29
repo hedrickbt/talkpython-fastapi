00aa98938be0913120179c08bd2f6e6d5008974d
bhedrick <brooke.t.hedrick@gmail.com>
2021-12-29 08:41:03 -0600

    - openweather api

--- ch05-a-realistic-api/services/openweather_service.py
+++ ch05-a-realistic-api/services/openweather_service.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 
 def get_report(city: str, state: Optional[str], country: str, units: str) -> dict:
-    key = 'b869fd2fdfedd184cea5ebe65289f96c'
+    key = 'nope'
     q = f'{city},{country}'
     url = f'https://api.openweathermap.org/data/2.5/weather?q={q}&appid={key}&units={units}'
     print(url)
