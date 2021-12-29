6e8aefba192ed0d7d7c0ddc3574ea43913c0ef03
bhedrick <brooke.t.hedrick@gmail.com>
2021-12-29 08:42:09 -0600

    Revert "- openweather api"
    This reverts commit 2ef7bf3231f2220016910c9ad6b249babd81d827.

--- ch05-a-realistic-api/services/openweather_service.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-
-
-def get_report(city: str, state: Optional[str], country: str, units: str) -> dict:
-    key = 'b869fd2fdfedd184cea5ebe65289f96c'
-    q = f'{city},{country}'
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={q}&appid={key}&units={units}'
-    print(url)
-
