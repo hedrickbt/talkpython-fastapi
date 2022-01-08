633ee7b6ebfa78933b14fceed0c62884382363a1
bhedrick <brooke.t.hedrick@gmail.com>
2022-01-08 10:51:16 -0600

    - add fall-through exception handling

--- ch06-error-handling-and-perf/api/weather_api.py
+++ ch06-error-handling-and-perf/api/weather_api.py
@@ -12,8 +12,12 @@ router = fastapi.APIRouter()
 
 @router.get('/api/weather/{city}')
 async def weather(loc: Location = Depends(),
-            units: Optional[str] = 'metric'):
+                  units: Optional[str] = 'metric'):
     try:
         return await openweather_service.get_report_async(loc.city, loc.state, loc.country, units)
     except ValidationError as ve:
         return fastapi.Response(content=ve.error_msg, status_code=ve.status_code)
+    except Exception as x:
+        # Easy to test, just disable your internet connection ( make sure you don't have a forecast cached )
+        print(f'Server crashed while processing request: {x}')
+        return fastapi.Response(content='Error processing your request.', status_code=500)
